c54061556e3619a6f750c61a10594509a10ddbac
Hwls_ek <nunnunan0321@naver.com>
2021-07-19 14:26:07 +0900

    프래그먼트 및 테마 설정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val  tab_layout: TabLayout =findViewById(R.id.tab_layout)
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                //val position = tab.position
+                when (tab_layout.selectedTabPosition) {
+
+                    0->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.frameLayout,MainFragment()).commitAllowingStateLoss()}
+                    }
+                    1-> {
+                        supportFragmentManager.beginTransaction().apply {
+                            replace(R.id.frameLayout,GroupFragment()).commitAllowingStateLoss()}
+                    }
+                    2->{
+                        supportFragmentManager.beginTransaction().apply {
+                            replace(R.id.frameLayout,SettingFragment()).commitAllowingStateLoss()}
+                    }
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SettingFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class SettingFragment: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_setting,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        class MainActivity:AppCompatActivity()
+
+
+    }
+
+
+
+}
